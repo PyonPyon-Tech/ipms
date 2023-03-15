17eb73d0ea696a60bb5f1e8fd73c7e26efe8f6a6
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-03-15 19:06:32 +0700

    chores: add color palette

--- frontend-service/tailwind.config.js
+++ frontend-service/tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     extend: {
       colors: {
         'regal-blue': '#243c5a',
+        'white': '#FCFCFC',
+        'grey': '#C5C5C5',
+        'grey-dark':'#A7A5A5',
+        'black': '1E1E1E',
+        'blue':'#006BD3',
+        'blue-light':'#4A9AE8',
+        'blue-dark':'#004F9C',
+        'teal':'#47BEAB',
+        'teal-light':'#6EE3D1',
+        'teal-dark':'#339082',
+        'coral':'#EF7373',
+        'coral-light':'#E8AAAA',
+        'coral-dark':'#CB4C4B',
+        'orange':'#F87E0D',
+        'orange-light':'#F3AD6B',
+        'orange-dark':'#C55E00',
       },
     }
   },
