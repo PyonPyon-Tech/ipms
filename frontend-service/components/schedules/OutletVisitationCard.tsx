5b87a58640eb308cf56d97c8e5721bd433912431
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-03-23 00:14:04 +0700

    feat(ajukan jadwal): add edit button

--- frontend-service/components/schedules/OutletVisitationCard.tsx
+++ frontend-service/components/schedules/OutletVisitationCard.tsx
@@ -1,7 +1,7 @@
 import { classNames } from "@functions/classNames";
 import { useScheduleForm } from "@hooks/useScheduleForm";
 import { OutletVisitations } from "@models/pestcontrol/outlets";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 
 export const OutletVisitationCard: FC<{ data: OutletVisitations }> = ({
   data,
@@ -46,27 +46,76 @@ export const OutletVisitationCard: FC<{ data: OutletVisitations }> = ({
           <h6 className="text-sm font-bold">Jadwal Kunjungan:</h6>
           <table className="my-1 table-auto text-xs">
             <tbody>
-              {data.visitations.map((x, idx) => {
-                return (
-                  <tr key={data.outletId+"outlet"+idx} >
-                    <td className="font-bold">{`Kunjungan ${idx+1} :`}</td>
-                    <td>
-                      <input
-                        onChange={e=>{
-                          changeVisitDate(data.outletId, idx, e.target.value)
-                        }}
-                        className="outletDate"
-                        type="date"
-                        defaultValue={x.date ?? ""}
-                      />
-                    </td>
-                  </tr>
-                );
-              })}
+              {data.visitations.map((x, idx) => (
+                <VisitationRow
+                  {...x}
+                  key={"vsrow" + data.outletId + "-" + x.id}
+                  changeVisitDate={changeVisitDate}
+                  data={data}
+                  index={idx}
+                />
+              ))}
             </tbody>
           </table>
         </div>
       )}
     </div>
+  );
+};
+
+const VisitationRow: FC<{
+  index: number;
+  id: number;
+  date: string;
+  data: OutletVisitations;
+  changeVisitDate: (outletId: number, index: number, date: string) => void;
+}> = ({ data, date, id, index, changeVisitDate }) => {
+  const [disabled, setDisabled] = useState(true);
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    if (!date) return;
+    setValue(date);
+  }, [date]);
+  return (
+    <tr key={data.outletId + "outlet" + index}>
+      <td className="font-bold">{`Kunjungan ${index + 1} :`}</td>
+      <td>
+        <input
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={disabled}
+          className="outletDate"
+          type="date"
+          value={value}
+        />
+      </td>
+      <td className="pl-4">
+        {disabled ? (
+          <div className="flex">
+            <div className="py-1.5 cursor-pointer px-3 bg-blue text-white rounded-md border-2 border-blue" onClick={() => setDisabled(false)}>
+              Edit
+            </div>
+          </div>
+        ) : (
+          <div className="flex gap-x-4">
+            <div className="py-1.5 cursor-pointer px-3 text-coral rounded-md border-2 border-coral" onClick={() => {
+              setValue(date)
+              setDisabled(true)
+            }}>
+              Batalkan
+            </div>
+            <div
+              className="py-1.5 cursor-pointer px-3 bg-teal text-white rounded-md border-2"
+              onClick={() => {
+                changeVisitDate(data.outletId, index, value);
+              }}
+            >
+              Simpan
+            </div>
+          </div>
+        )}
+      </td>
+    </tr>
   );
 };
