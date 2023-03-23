e4f9b6c0857606fc83d47b6a1e80d54e9aeca4a7
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-03-23 11:29:52 +0700

    fix: add region spv on outlet

--- frontend-service/components/customers/OutletForm/index.tsx
+++ frontend-service/components/customers/OutletForm/index.tsx
@@ -3,10 +3,7 @@ import { AxiosClient, URL_CUSTOMER, URL_EMPLOYEE } from "@constants/api";
 import { PATH_ROLES } from "@constants/roles";
 import { useAuth } from "@hooks/useAuth";
 import { Outlet, OutletMutation } from "@models/customer/outlet";
-import {
-  OutletFields,
-  OutletFormFactory,
-} from "@models/customer/outlet/form";
+import { OutletFields, OutletFormFactory } from "@models/customer/outlet/form";
 import { EmployeeClass } from "@models/pestcontrol/employee";
 import { EmployeeSupervisor } from "@models/pestcontrol/employee/form";
 import { AxiosError } from "axios";
@@ -16,22 +13,21 @@ import { FC, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
-export const OutletForm: FC<{ }> = ({  
-}) => {
+export const OutletForm: FC<{}> = ({}) => {
   const router = useRouter();
   const { register, handleSubmit } = useForm<OutletFields>();
-  const [ supervisors, setSupervisors ] = useState<EmployeeSupervisor[]>([]);
+  const [supervisors, setSupervisors] = useState<EmployeeSupervisor[]>([]);
 
   useEffect(() => {
     AxiosClient.get(`${URL_EMPLOYEE}/supervisors`)
-    .then((response) => {
-      setSupervisors(response.data.map((x: any) => new EmployeeClass(x)));
-    })
-    .catch((err: AxiosError) => {
-      console.log(err);
-    });
+      .then((response) => {
+        setSupervisors(response.data.map((x: any) => new EmployeeClass(x)));
+      })
+      .catch((err: AxiosError) => {
+        console.log(err);
+      });
   }, [router]);
-  
+
   const onSubmit = async (data: OutletFields) => {
     let outlet: OutletMutation | null = null;
 
@@ -39,14 +35,11 @@ export const OutletForm: FC<{ }> = ({
     outlet.customer.id = parseInt(router.query.id as string);
     const customerId = outlet.customer.id;
 
-    AxiosClient.post(
-      `${URL_CUSTOMER}/${customerId}/outlets`,
-      outlet
-    )
+    AxiosClient.post(`${URL_CUSTOMER}/${customerId}/outlets`, outlet)
       .then((response) => {
         console.log(response.data);
         toast.success("Sukses membuat outlet", {
-          duration: 5000
+          duration: 5000,
         });
         router.push(`/customers/${customerId}`);
       })
@@ -71,7 +64,9 @@ export const OutletForm: FC<{ }> = ({
         <input required {...register("address")} />
         <h5>Supervisor</h5>
         <select {...register("supervisorId")}>
-            {supervisors.map((supervisor) => <option value={supervisor.id} key={supervisor.id}>{supervisor.user.name}</option>)}
+          {supervisors.map(({ id, user: { name }, region }) => (
+            <option value={id} key={id}>{`${name} (${region})`}</option>
+          ))}
         </select>
 
         <button
