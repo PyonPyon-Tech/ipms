dc783aa8bb77d975b9b87fbd7fc7f3af1b23a8f9
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-04-08 22:07:05 +0700

    feat(frontend): add interfaces

---
+++ frontend-service/utils/models/report/CsrQuestion/CsrFindingArea.ts
@@ -0,0 +1,13 @@
+export interface CsrFindingArea{
+    id: number;
+    area: CsrArea;
+    displayNumber: string;
+    question: string;
+    recommendation: string[];
+}
+
+export interface CsrArea {
+    id: number;
+    area: string;
+  }
+  
---
+++ frontend-service/utils/models/report/CsrReport.ts
@@ -0,0 +1,37 @@
+import { Outlet } from "@models/customer/outlet";
+import { Period } from "@models/period";
+import { Employee } from "@models/pestcontrol/employee";
+import { EmployeeTechnician } from "@models/pestcontrol/employee/form";
+import { User } from "@models/user";
+import { CsrDetailArea } from "./CsrAnswer/CsrDetailArea";
+import { CsrDetailPest } from "./CsrAnswer/CsrDetailPest";
+import { CsrPesticideUsage } from "./CsrAnswer/CsrPesticideUsage";
+
+export interface CsrReport {
+    id: number;
+    reportType: number;
+    visitationType: number;
+    feedback: object;
+    period: Period;
+    technician: EmployeeTechnician;
+    outlet: OutletExtended,
+    start: string;
+    end: string;
+    technicianSignature: string;
+    picSignature: string;
+    visitationPhoto: string;
+    detailAreas: CsrDetailArea[];
+    detailPests: CsrDetailPest[];
+    pesticideUsages: CsrPesticideUsage[];
+}
+
+interface OutletExtended extends Outlet {
+    customer: {
+        id: number;
+        user: User;
+        contact: string;
+        visitationFrequency: number;
+        startContract: string;
+        endContract: string;
+    }
+}
