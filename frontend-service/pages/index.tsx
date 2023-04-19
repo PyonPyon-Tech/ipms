9867738bc98bcd126f52b904d5c907fa21b6342e
GhaitsaDina <ghaitsamaulidina@gmail.com>
2023-04-19 21:46:18 +0700

    chore: button component

--- frontend-service/pages/index.tsx
+++ frontend-service/pages/index.tsx
@@ -2,16 +2,18 @@ import { withAuth } from "@functions/withAuth";
 import { withLayout } from "@functions/withLayout";
 import { NextPage } from "next";
 import { Button } from "@components/general/Button";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const router = useRouter();
     return (
         <div className="mb-4 w-full pr-8 md:pr-12 md:pt-0">
             <Button
+                className='bg-orange'
                 action={{
                     name: "Tambah Chemical Baru",
-                    path: `/inventories/add`,
-                    bgColor: 'coral',
-                    submit: true
+                    submit: true,
+                    func:() => router.push('/inventories/add'),
                 }}
             ></Button>
         </div>
