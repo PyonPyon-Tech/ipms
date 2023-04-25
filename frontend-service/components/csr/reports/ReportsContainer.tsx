7caecba02fab793e8e79e34f83094816868f1009
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-04-25 16:48:42 +0700

    fix(report): duplicate card view report

--- frontend-service/components/csr/reports/ReportsContainer.tsx
+++ frontend-service/components/csr/reports/ReportsContainer.tsx
@@ -7,18 +7,22 @@ import { FC, useState } from "react";
 import { TechnicianReportsCard } from "./ReportCard";
 import { Checkbox } from "@mui/material";
 
-export const ReportContainer : FC<{data: CsrReportClass[], count: number}> = ({data, count})=>{
-    const [searchTerm, setSearchTerm] = useState("")
-    return <section>
+export const ReportContainer: FC<{ data: CsrReportClass[]; count: number }> = ({ data, count }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <section>
       {/* <Search setSearchTerm={setSearchTerm} placeholder="Nama Customer atau Laporan" /> */}
       <div className="flex flex-row items-center space-x-4">
-        <h3 className="text-lg font-semibold my-2">Ditemukan {count} laporan</h3>
+        <h3 className="my-2 text-lg font-semibold">Ditemukan {count} laporan</h3>
         {/* <Checkbox/> */}
         {/* <h4>0 Selected</h4> */}
         {/* <img className="object-fill h-6 w-6" src="/icons/download.svg" /> */}
       </div>
-      {filterDataOnlyNested<CsrReportClass, OutletClass>(data, "outlet", searchTerm, ["name"]).map(obj=>{
-        return <TechnicianReportsCard key={`report-card-${obj.id}`} data={obj} />
-      })}
+      <div>
+        {data.map((obj) => {
+          return <TechnicianReportsCard key={`report-card-${obj.id}`} data={obj} />;
+        })}
+      </div>
     </section>
-}
+  );
+};
