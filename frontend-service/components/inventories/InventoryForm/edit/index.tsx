69eaeedb220c94953ccaf5210f54c3043db4f2ba
GhaitsaDina <ghaitsamaulidina@gmail.com>
2023-04-26 22:37:20 +0700

    chore: add constraint for negative value in stok field

--- frontend-service/components/inventories/InventoryForm/edit/index.tsx
+++ frontend-service/components/inventories/InventoryForm/edit/index.tsx
@@ -7,65 +7,65 @@ import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 
 export const InventoryEditStock: FC<Pesticide> = ({
-    id,
-    name,
-    activeIngredient,
-    stock,
-    targets,
-    unit,
-    targetPests,
+  id,
+  name,
+  activeIngredient,
+  stock,
+  targets,
+  unit,
+  targetPests,
 }) => {
-    const router = useRouter();
-    const { register, handleSubmit } = useForm();
-    const onSubmit = async (data: any) => {
-        console.log(data);
-        const submittedData = {
-            name: data.name,
-            stock: Number(data.stock),
-            activeIngredient: data.activeIngredient,
-            unit: data.unit,
-            targets: data.targets,
-            targetPests: targetPests,
-        };
-        AxiosClient.put(
-            `${URL_INVENTORY}/pesticides/${router.query.id}`,
-            submittedData
-        )
-            .then((response) => {
-                console.log(response.data);
-                toast.success("Sukses mengedit pestisida", {
-                    duration: 5000,
-                });
-                router.push(`/inventories/${router.query.id}`);
-            })
-            .catch((error) => {
-                toast.error(error.message);
-                console.log(error);
-            });
+  const router = useRouter();
+  const { register, handleSubmit } = useForm();
+  const onSubmit = async (data: any) => {
+    console.log(data);
+    const submittedData = {
+      name: data.name,
+      stock: Number(data.stock),
+      activeIngredient: data.activeIngredient,
+      unit: data.unit,
+      targets: data.targets,
+      targetPests: targetPests,
     };
-    return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex">
-                    <div className="round my-2 flex w-1/2 flex-col gap-2 rounded-l-[8px] bg-orange-light p-4 align-middle">
-                        <h5 className="text-base font-bold">Stok Sekarang</h5>
-                        <p className="pb-1 text-base">{stock}</p>
-                    </div>
-                    <div className="my-2 flex w-1/2 flex-col gap-2 rounded-r-[8px] bg-teal-light p-4 align-middle">
-                        <h5 className="text-base font-bold">Stok Terbaru</h5>
+    if (submittedData.stock < 0) {
+      toast.error("Stok tidak boleh kurang dari 0!");
+    } else {
+      AxiosClient.put(
+        `${URL_INVENTORY}/pesticides/${router.query.id}`,
+        submittedData
+      )
+        .then((response) => {
+          console.log(response.data);
+          toast.success("Sukses mengedit pestisida", {
+            duration: 5000,
+          });
+          router.push(`/inventories/${router.query.id}`);
+        })
+        .catch((error) => {
+          toast.error(error.message);
+          console.log(error);
+        });
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex">
+          <div className="round my-2 flex w-1/2 flex-col gap-2 rounded-l-[8px] bg-orange-light p-4 align-middle">
+            <h5 className="text-base font-bold">Stok Sekarang</h5>
+            <p className="pb-1 text-base">{stock}</p>
+          </div>
+          <div className="my-2 flex w-1/2 flex-col gap-2 rounded-r-[8px] bg-teal-light p-4 align-middle">
+            <h5 className="text-base font-bold">Stok Terbaru</h5>
 
-                        <input
-                            className="pb-1"
-                            type="number"
-                            {...register("stock")}
-                        />
-                    </div>
-                </div>
-                <Button
-                    className="w-full"
-                    action={{ name: "Simpan", submit: true }}
-                ></Button>
-            </form>
+            <input className="pb-1" type="number" {...register("stock")} />
+          </div>
         </div>
-    );
+        <Button
+          className="w-full"
+          action={{ name: "Simpan", submit: true }}
+        ></Button>
+      </form>
+    </div>
+  );
 };
